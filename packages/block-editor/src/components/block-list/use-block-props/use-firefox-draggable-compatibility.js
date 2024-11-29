76f61df275ff88b88a5a9a1bbfdcf6b1bf2087c1
Ella <ella@vandurpe.com>
2024-11-29 12:07:55 +0100

    add ff comment

--- packages/block-editor/src/components/block-list/use-block-props/use-firefox-draggable-compatibility.js
+++ packages/block-editor/src/components/block-list/use-block-props/use-firefox-draggable-compatibility.js
@@ -3,6 +3,14 @@
  */
 import { useRefEffect } from '@wordpress/compose';
 
+/**
+ * In Firefox, the `draggable` and `contenteditable` attributes don't play well
+ * together. When `contenteditable` is within a `draggable` element, selection
+ * doesn't get set in the right place. The only solution is to temporarily
+ * remove the `draggable` attribute clicking inside `contenteditable` elements.
+ *
+ * @return {Function} Cleanup function.
+ */
 export function useFirefoxDraggableCompatibility() {
 	return useRefEffect( ( node ) => {
 		function onDown( event ) {
